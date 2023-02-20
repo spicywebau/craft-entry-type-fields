2d5662b76822c21d200067c331c321bd7e1fa0d0
Thomas Templeton <thomas.templeton.dev@gmail.com>
2023-02-20 18:47:30 +1100

    Plugin now installable

---
+++ src/Plugin.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace spicyweb\entrytypesfield;
+
+use craft\base\Plugin as BasePlugin;
+
+/**
+ * Main Entry Types Field plugin class.
+ *
+ * @package spicyweb\entrytypesfield
+ * @author Spicy Web <plugins@spicyweb.com.au>
+ * @since 1.0.0
+ */
+class Plugin extends BasePlugin
+{
+}
